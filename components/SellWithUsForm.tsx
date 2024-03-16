a240907e015119408fd9e81388dd75e69ce3f5cb
Osaretin Frank <osaretin.frank10@gmail.com>
2024-03-16 03:39:02 +0100

    add tabs

--- components/SellWithUsForm.tsx
+++ components/SellWithUsForm.tsx
@@ -1,5 +1,16 @@
 "use client"
 import React from "react"
+import { TabProps, Tabs } from "./Tabs"
+const items: TabProps[] = [
+  {
+    title: "1/2. Product Details",
+    content: <form>Hey</form>,
+  },
+  {
+    title: "2/2. Seller Information",
+    content: <form>Hey</form>,
+  },
+]
 
 export const SellWithUsForm = () => {
   return (
@@ -7,11 +18,13 @@ export const SellWithUsForm = () => {
       id="sell-with-us"
       className="md:py-[0rem] pt-9 mb-[2rem] md:mb-[3rem] md:mt-[4rem] relative overflow-hidden reveal-down"
     >
-      <div className="w-[90%] mx-auto md:w-[90%] lg:w-[40%] 3xl:w-[40%]">
+      <div className="w-[90%] mx-auto md:w-[90%] lg:w-[80%] 3xl:w-[70%]">
         <h2 className="md:text-[48px] text-[24px] pb-[1rem] md:pb-[2rem] text-center">
           Sell With Us Form
         </h2>
-        <div></div>
+        <div>
+          <Tabs items={items} />
+        </div>
       </div>
     </div>
   )
---
+++ components/Tabs.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React, { useState } from "react"
+
+export interface TabProps {
+  title: string
+  content: React.ReactNode
+}
+
+export interface props {
+  items: TabProps[]
+}
+
+export const Tabs: React.FC<props> = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState<number>(0)
+  const titles = items.map((item) => item.title)
+  return (
+    <div className="border-2 border-grey-400 rounded-xl">
+      <div className="flex justify-between">
+        {titles.map((title, index) => (
+          <div
+            key={title}
+            onClick={() => setActiveIndex(index)}
+            className={`w-full text-center p-5 border-b-2 transition-all duration-10 cursor-pointer text-[14px] md:text-[20px] ${
+              activeIndex === index
+                ? "text-black font-semibold border-primary"
+                : "text-[#98999A] border-grey-400"
+            }`}
+          >
+            <span>{title}</span>
+          </div>
+        ))}
+      </div>
+      <div className="p-5">{items[activeIndex].content}</div>
+    </div>
+  )
+}
