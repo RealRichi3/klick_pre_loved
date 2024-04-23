77808f657cc60bb87e1c5e2de3711b0c7bd85ca7
RealRichi3 <molunorichie@gmail.com>
2024-04-23 14:15:46 +0100

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Klick",
-  description: "Built with Next",
+    title: "Klick",
+    description: "Built with Next",
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <Head>
+                <link rel="icon" href="/public/favicon.ico" type="image/x-icon" sizes="1364x1364" />
+            </Head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
